ac3281ff5cd4dbd6659b513486b5c7ac7e48d4c0
COAOX <windowsxu563504792@live.com>
2020-02-05 12:50:57 +0800

    update

--- dataclean.py
+++ dataclean.py
@@ -1,24 +1,6 @@
 import pickle
 import os
-train = {}
-test = {}
-with open('cifar100/train','rb') as f:
-    train = pickle.load(f, encoding='latin1')
-train_data = train['data']
-train_labels = train['fine_labels']
-res_data = []
-res_labels = []
-for train_d, train_l in zip(train_data, train_labels):
-	if not choice(train_label):
-		continue
-    res_data.append(train_d)
-    res_labels.append(train_l)
-train['data'] = res_data
-train['fine_labels'] = res_labels
-with open('cifar100/train','wb') as f:
-	pickle.dump(train, f)
-
-
+import random
 def choice(i):
     reed = random.random()
     ind = random.random()
@@ -31,3 +13,23 @@ def choice(i):
     if reed<=n:
         return True
     return False
+
+train = {}
+test = {}
+
+with open('cifar100/train','rb') as f:
+    train = pickle.load(f, encoding='latin1')
+train_data = train['data']
+train_labels = train['fine_labels']
+res_data = []
+res_labels = []
+for train_d, train_l in zip(train_data, train_labels):
+	if not choice(train_l):
+		continue
+	res_data.append(train_d)
+	res_labels.append(train_l)
+train['data'] = res_data
+train['fine_labels'] = res_labels
+with open('cifar100/train','wb') as f:
+	pickle.dump(train, f)
+
